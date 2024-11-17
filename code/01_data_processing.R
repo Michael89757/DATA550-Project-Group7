09b6f747d746fe73bc38f1b660f44f2cd09a89be
Leona Qin <hqin34@emory.edu>
2024-11-16 19:19:15 -0500

    add data processing and table1

--- code/01_data_processing.R
+++ code/01_data_processing.R
@@ -6,6 +6,9 @@ absolute_path_to_data <- here::here("data", "covid_sub.csv")
 df <- read.csv(absolute_path_to_data, header = TRUE)
 
 # Coder 1 TO DO Here
+data$COVID19_CARRIER_STATUS <- ifelse(data$CLASIFFICATION_FINAL >= 1 & data$CLASIFFICATION_FINAL <= 3, 
+                                      "Confirmed", "Unconfirmed")
+data$DEATH <- ifelse(is.na(data$DATE_DIED), "No", "Yes")
 
 
 # save cleaned data in `data/` folder
--- code/02_make_table1.R
+++ code/02_make_table1.R
@@ -7,10 +7,19 @@ df <- readRDS(
 )
 
 # Coder 1 TO DO Here
+library(gtsummary)
 
+table_one <- data |>
+  select("SEX", "AGE", "COVID19_CARRIER_STATUS", "PATIENT_TYPE", "PNEUMONIA", "PREGNANT", "DIABETES", 
+         "COPD", "ASTHMA", "INMSUPR", "HIPERTENSION", "CARDIOVASCULAR", "RENAL_CHRONIC", "OTHER_DISEASE",
+         "OBESITY", "TOBACCO", "USMER", "MEDICAL_UNIT", "INTUBED", "ICU", "DEATH") |>
+  tbl_summary(by = DEATH, missing = "no") |>
+  modify_spanning_header(c("stat_1", "stat_2") ~ "**DEATH**") |>
+  add_overall() |>
+  add_p()
 
 saveRDS(
-  table1,
+  table_one,
   file = here::here("table/table1.rds")
 )
 
