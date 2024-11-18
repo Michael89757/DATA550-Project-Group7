1033f32167639609f1bc557a082d34f2b1fc3797
Michael89757 <xinyu.zhang2@outlook.com>
2024-11-17 22:44:43 -0500

    added two regression models to the code and with one full model and a optimized model with DIABETES, CARDIOVASCULAR, OBESITY, MEDICAL_UNIT removed due to insignificant effects, saved the optimized model in the output.

--- code/05_survival_analysis.R
+++ code/05_survival_analysis.R
@@ -1,9 +1,71 @@
 here::i_am("code/05_survival_analysis.R")
 
+library(dplyr)
+library(gtsummary)
 df <- readRDS(
   file = here::here("data/covid_data_cleaned.rds")
 )
 
-# Coder 4 TO DO Here
 
-# save results in `output/` folder
+
+
+
+df_regression<-df%>%
+  select(-X,-DATE_DIED)%>%
+  mutate(ICU = case_when(
+    ICU == "Yes" ~ "Yes",
+    ICU == "No" | is.na(ICU) ~ "No/NA"
+  ))%>%
+  mutate(PREGNANT = case_when(
+    PREGNANT == "Yes" ~ "Yes",
+    PREGNANT == "No" | is.na(PREGNANT) ~ "No/NA"
+  ))%>%
+  mutate(INTUBED  = case_when(
+    INTUBED  == "Yes" ~ "Yes",
+    INTUBED  == "No" | is.na(INTUBED ) ~ "No/NA"
+  ))
+
+df_regression$DEATH <- ifelse(df_regression$DEATH == "Yes", 1, 0)
+df_regression <- na.omit(df_regression)
+
+
+# Coder 4 TO DO Here
+# Fit the logistic regression model
+full_model <- glm(DEATH ~ factor(USMER) + factor(MEDICAL_UNIT) +
+               factor(SEX) + factor(PATIENT_TYPE) + factor(INTUBED) + factor(PNEUMONIA) +
+               AGE + factor(PREGNANT) + factor(DIABETES) +  factor(INMSUPR) + factor(HIPERTENSION) +
+               factor(OTHER_DISEASE) + factor(CARDIOVASCULAR) + factor(OBESITY) +
+               factor(RENAL_CHRONIC) + factor(TOBACCO) + factor(CLASIFFICATION_FINAL) +
+               factor(ICU),
+             data = df_regression,
+             family = "binomial")
+
+# View the summary of the model
+summary(full_model)
+
+
+
+
+
+optimized_model <- glm(DEATH ~ factor(USMER) +
+                factor(SEX) + factor(PATIENT_TYPE) + factor(INTUBED) + factor(PNEUMONIA) +
+                AGE +  factor(DIABETES)  + factor(INMSUPR) + factor(HIPERTENSION) +
+                factor(OTHER_DISEASE) + factor(RENAL_CHRONIC) + factor(TOBACCO) + factor(CLASIFFICATION_FINAL) +
+                factor(ICU),
+                  data = df_regression,
+                  family = "binomial")
+
+# View the summary of the model
+summary(optimized_model)
+
+optimized_regression_table<-
+  tbl_regression((optimized_model),exponentiate=TRUE)
+
+optimized_regression_table
+
+
+saveRDS(
+  optimized_regression_table,
+  file = here::here("output","optimized_model.rds")
+)
+
